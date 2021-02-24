bdac7b008e2b33f6760b4f72b9d2d7d6335bfb2c
JavaMFadlan <polisilol48@gmail.com>
2021-02-24 08:36:35 +0700

    heroku hosting

---
+++ database/migrations/2021_01_14_064933_create_tracking_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrackingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('id_rw')->constrained('rws')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('sembuh');
+            $table->integer('positif');
+            $table->integer('meninggal');
+            $table->date('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracking');
+    }
+}
