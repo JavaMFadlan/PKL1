636226f2c50ffda16836585cf1b5a49920e400ea
JavaMFadlan <polisilol48@gmail.com>
2021-02-26 09:33:34 +0700

    m

--- app/tracking.php
+++ app/tracking.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class tracking extends Model
 {
     protected $fillable = ['sembuh','positif','meninggal'];
-    // public $timestamps = true;
+    public $timestamps = true;
     public function rw()
     {
         return $this->belongsTo('App\rw', 'id_rw');
