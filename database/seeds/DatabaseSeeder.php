8e7694ae01e395f944d30c363c6cc2723af0a121
JavaMFadlan <polisilol48@gmail.com>
2021-02-26 07:55:23 +0700

    H

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(Kecamatan::class);
-        $this->call(Kota::class);
         $this->call(Provinsi::class);
+        $this->call(Kota::class);
+        $this->call(Kecamatan::class);
     }
 }
